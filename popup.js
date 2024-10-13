01c6be6e38a538b7aa037e142d17c3bed2230a4c
Stuti Sharma <stuti105sharma@gmail.com>
2024-10-13 09:50:21 +0530

    popup.js

---
+++ popup.js
@@ -0,0 +1,175 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document
+    .getElementById("accountList")
+    .addEventListener("click", changeAccount);
+
+    document
+    .getElementById("userAddress")
+    .addEventListener("click", copyAddress);
+
+    document
+    .getElementById("transferFund")
+    .addEventListener("click", handler)
+
+    document
+    .getElementById("header_network")
+    .addEventListener("click", getOpenNetwork)
+
+    document
+    .getElementById("network_item")
+    .addEventListener("click", getSelectedNetwork)
+
+    document
+    .getElementById("add_network")
+    .addEventListener("click", setNetwork)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("accountCreate")
+    .addEventListener("click", createUser)
+
+    document
+    .getElementById("openCreate")
+    .addEventListener("click", openCreate)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+
+    document
+    .getElementById("loginAccount")
+    .addEventListener("click", loginUser)
+})
+
+// state variable
+let provideURL = "https://polygon-amoy.g.alchemy.com/v2/xw5NYg45EfnYqKKWu3OKuElPDkQwEj8K";
+
+let provider;
+let privatekey;
+let address;
+
+// function
+function handler() {
+    document.getElementById("transfer_center").style.display = "flex";
+
+    const amount = document.getElementById("amount").value;
+    const address = document.getElementById("address").value;
+
+    
+}
+
+function checkBalance() {
+
+}
+
+function getOpenNetwork() {
+
+}
+
+function setNetwork() {
+
+}
+
+function getSelectedNetwork() {
+
+}
+
+function loginUser() {
+
+}
+
+function openCreate() {
+
+}
+
+function signUp() {
+
+}
+
+function login() {
+
+}
+
+function logout() {
+
+}
+
+function openTransfer() {
+
+}
+
+function goBack() {
+
+}
+
+function openImport() {
+
+}
+
+function importGoBack() {
+
+}
+
+function openActivity() {
+
+}
+
+function openAssets() {
+
+}
+
+function goHomePage() {
+
+}
+
+function openImportModel() {
+
+}
+
+function closeImportModel() {
+
+}
+
+function addToken() {
+
+}
+
+function addAccount() {
+
+}
+
+function myFunction() {
+
+}
+
+function copyAddress() {
+
+}
+
+function changeAccount() {
+
+}
+
